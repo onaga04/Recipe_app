8782c408694a2b646e54d1539cc3c7132cf0a004
Ebube Onaga <31213368+onaga04@users.noreply.github.com>
2023-06-12 15:18:41 -0500

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -1,31 +1,34 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 recipes = [
-    {'title': 'Pasta Carbonara', 'ingredients': ['pasta', 'eggs', 'bacon'], 'instructions': '...'},
-    {'title': 'Chicken Curry', 'ingredients': ['chicken', 'curry powder', 'coconut milk'], 'instructions': '...'},
-    # Add more recipes as needed
+    {
+        'title': 'Pasta Carbonara',
+        'ingredients': ['spaghetti', 'bacon', 'eggs', 'cheese', 'black pepper'],
+        'instructions': 'Lorem ipsum dolor sit amet...'
+    },
+    {
+        'title': 'Chicken Parmesan',
+        'ingredients': ['chicken breasts', 'breadcrumbs', 'marinara sauce', 'mozzarella cheese'],
+        'instructions': 'Lorem ipsum dolor sit amet...'
+    },
+    # Add more recipe data as needed
 ]
 
-# Endpoint to get all recipes
-@app.route('/api/recipes', methods=['GET'])
-def get_recipes():
-    return jsonify(recipes)
+@app.route('/')
+def home():
+    return render_template('index.html', recipes=recipes)
 
-# Endpoint to search for recipes
-@app.route('/api/recipes/search', methods=['GET'])
-def search_recipes():
-    recipe_title = request.args.get('title')
-    matching_recipes = [recipe for recipe in recipes if recipe_title.lower() in recipe['title'].lower()]
-    return jsonify(matching_recipes)
+@app.route('/recipe/<title>')
+def recipe(title):
+    # Find the recipe with the given title
+    recipe = next((r for r in recipes if r['title'].lower() == title.lower()), None)
 
-# Endpoint to add a new recipe
-@app.route('/api/recipes', methods=['POST'])
-def add_recipe():
-    new_recipe = request.get_json()
-    recipes.append(new_recipe)
-    return jsonify(new_recipe)
+    if recipe:
+        return render_template('recipe.html', recipe=recipe)
+    else:
+        return 'Recipe not found'
 
 if __name__ == '__main__':
     app.run(debug=True)
